100355afc6c7c92de0fa89b53b703a060fdf6fc3
netravler <pheintz@hotmail.com>
2018-09-13 14:00:07 -0500

    Add files via upload

---
+++ XMLrIpper/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace XMLrIpper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tbXMLOutput = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tbXMLOutput
+            // 
+            this.tbXMLOutput.Location = new System.Drawing.Point(16, 208);
+            this.tbXMLOutput.Multiline = true;
+            this.tbXMLOutput.Name = "tbXMLOutput";
+            this.tbXMLOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbXMLOutput.Size = new System.Drawing.Size(905, 292);
+            this.tbXMLOutput.TabIndex = 0;
+            this.tbXMLOutput.Click += new System.EventHandler(this.tbXMLOutput_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(819, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File to Rip";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 512);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbXMLOutput);
+            this.Name = "Form1";
+            this.Text = "XMLrIpper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tbXMLOutput;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ XMLrIpper/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace XMLrIpper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+
+        }
+
+        // xml parse junk here!
+
+        public void Read(string fileName)
+        {
+            XDocument doc = XDocument.Load(fileName);
+
+            foreach (XElement el in doc.Root.Elements())
+            {
+                Console.WriteLine("{0} {1}", el.Name, el.Attribute("id").Value);
+                Console.WriteLine("  Attributes:");
+                foreach (XAttribute attr in el.Attributes())
+                    Console.WriteLine("    {0}", attr);
+                Console.WriteLine("  Elements:");
+
+                foreach (XElement element in el.Elements())
+                    Console.WriteLine("    {0}: {1}", element.Name, element.Value);
+            }
+        }
+
+        private void tbXMLOutput_Click(object sender, EventArgs e)
+        {
+            Read(@textBox1.Text);
+        }
+    }
+}
